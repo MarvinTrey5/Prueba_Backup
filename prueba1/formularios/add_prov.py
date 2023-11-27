ceb51a9b71712e233140981882f0d7922ca12243
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-27 14:25:16 -0600

    Clase creada, Añadir Proveedor Utilizando Forms/Form

--- prueba1/formularios/add_prov.py
+++ prueba1/formularios/add_prov.py
@@ -1,4 +1,6 @@
 from django import forms
+# Clase para añadir proveedor
+# y se llama en la view.py 
 class Add_prov(forms.Form):
     nombre=forms.CharField(max_length=100)
     telefono=forms.CharField(max_length=8)
