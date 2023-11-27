f005e4bba81c17187bb4139b687a482c47e3de49
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-27 11:43:37 -0600

    Modelos a visualizar en pgAdmin

--- prueba1/admin.py
+++ prueba1/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Proveedores,Productos
 # Register your models here.
+# Para poder visualizar los datos al ingresar como pgAdmin
 admin.site.register(Proveedores)
 admin.site.register(Productos)
