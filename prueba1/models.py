2fccf9786f9423d12a830fea9781acfcadb49a7e
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-27 11:46:50 -0600

    Modelos de la base de datos

--- prueba1/models.py
+++ prueba1/models.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import Group
 Group.objects.get_or_create(name='Cajero')
 Group.objects.get_or_create(name='Estudiante')
 # Create your models here.
+# Modelo para la tabla de Proveedores.
 class Proveedores(models.Model):
     nombre=models.CharField(max_length=100)
     telefono=models.CharField(max_length=8)
     def __str__(self):
         return self.nombre
+# Modelo para la tabla de productos.
 class Productos(models.Model):
     nombre = models.CharField(max_length=100)
     stock = models.PositiveIntegerField()
-    fk_prov=models.ForeignKey(Proveedores, on_delete=models.CASCADE)
+    fk_prov=models.ForeignKey(Proveedores, on_delete=models.CASCADE) # Se relacionará con el id de la tabla Proveedores
     def __str__(self):
         return self.nombre
