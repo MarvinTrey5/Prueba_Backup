2d0690372045beb8937f606421cbc38df7b8099a
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-27 14:26:24 -0600

    Clase creada, Utilizando Meta Obteniendo username / password

--- prueba1/formularios/loginform.py
+++ prueba1/formularios/loginform.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
+# Clase para acceder al login obteniendo
+# su usuario y su contraseña.
+# y se llama en la views.py
 class LoginForm(AuthenticationForm):
     class Meta:
         model=User
