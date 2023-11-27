c1d51d7513bdb0e9d19cbebcd323476aea12d815
MarvinTrey5 <santosmarvin201@gmail.com>
2023-11-27 14:25:00 -0600

    Clase creada, Añadir Producto Utilizando Forms/Form

--- prueba1/formularios/add_prod.py
+++ prueba1/formularios/add_prod.py
@@ -1,6 +1,7 @@
 from django import forms
-
+# Clase para añadir productos
+# y se llama en la view.py 
 class Add_prod(forms.Form):
     nombre = forms.CharField(max_length=100)
     stock = forms.IntegerField()
-    fk_prov=forms.IntegerField()
+    fk_prov=forms.IntegerField() # Llave foránea que se relaciona con el id de la tabla Proveedor.
